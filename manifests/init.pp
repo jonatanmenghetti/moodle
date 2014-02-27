c0b04ad3dbceab371cdde6c7193e949a20cf8ac7
Mark Greenhalgh <mgreenha@yahoo.co.uk>
2014-02-27 15:18:19 +0000

    Added apache vhost and other improments

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,13 +11,15 @@ class moodle (
   $dirtree = dirtree('/var/www/html')
   ensure_resource('File', $dirtree, {
     ensure => directory,
-    mode   => '0755',
+#    mode   => '0755',
   })
 
 # git clone moodle
   exec { 'Base clone':
     command => 'git clone git@github.com:moodle/moodle.git',
-  # onlyif => 'Somefile does not exist',
+    cwd => '/var/www/html',
+    path => '/usr/bin',
+    creates => '/var/www/html/moodle',
   }
 
 # create data directory
@@ -28,8 +30,28 @@ class moodle (
   })
 
 # get database info
-# create config.php
-# apache vhost
-# set up cron
 
+# create config.php
+  ensure_resource('File', '/var/www/html/moodle/config.php', {
+    ensure => present,
+    source => 'puppet:///modules/moodle/config.php',
+  })
+
+# apache vhost
+  class { 'apache':}
+  apache::vhost {'moodle':
+    port => '80',
+    docroot => '/var/www/html/moodle',
+    aliases => {
+      alias => '/mymoodle',
+      path => '/var/www/html/moodle',
+    }
+  }
+
+# set up cron
+  cron { moodle:
+    command => '/usr/bin/wget -O /dev/null http://localhost/mymoodle/admin/cron.php',
+    user => root, # moodle user
+    minute => '*/5',
+  }
 }
