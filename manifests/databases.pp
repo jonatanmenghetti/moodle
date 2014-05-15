0b67946eee5eb656efdb883f5ab94a5ba5776077
Mark Greenhalgh <mgreenha@yahoo.co.uk>
2014-05-15 16:29:55 +0100

    Rake

--- manifests/databases.pp
+++ manifests/databases.pp
@@ -9,7 +9,7 @@ class moodle::databases (
         package{'php-pear-MDB2-Driver-mysqli': ensure => installed}
         create_resources('class', $::moodle::params::moodle_mysql)
       }
-      postgresql: { include moodle::databases::postgresql }  
+      postgresql: { include moodle::databases::postgresql }
       default: { notify{"${database} is not yet supported":}}
     }
   }
