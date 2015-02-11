06e1a64b502f1f41b2fc9dd74d59f5e276ac3bf9
Firebladee <mgreenha@yahoo.co.uk>
2015-02-11 17:04:23 +0000

    Start of moving variables to autolookup

--- manifests/web/apache1.pp
+++ manifests/web/apache1.pp
@@ -1,11 +1,17 @@
 
 
-class moodle::web::apache1 {
+class moodle::web::apache1 (
+  $vhost_port    = '80',
+  $vhost_docroot = '/var/www/html/moodle',
+
+# $apache_mod = array
+# $php_mod = array
+){
 
   class { 'apache': }
   apache::vhost {'moodle':
-    port    => '80',
-    docroot => '/var/www/html/moodle',
+    port    => $vhost_port,
+    docroot => $vhost_docroot,
     aliases => {
       alias   => '/mymoodle',
       path    => '/var/www/html/moodle',
