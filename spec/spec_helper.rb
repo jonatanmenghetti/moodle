8d107563471f712f88be186d900a30275b4fc2e6
Firebladee <mgreenha@yahoo.co.uk>
2015-04-16 16:36:33 +0100

    Getting rspec to work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,5 @@
-require 'rspec-puppet'
+require 'puppetlabs_spec_helper/module_spec_helper'
 require 'coveralls'
 
 Coveralls.wear!
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
-
-RSpec.configure do |c|
-  c.module_path = File.join(fixture_path, 'modules')
-  c.manifest_dir = File.join(fixture_path, 'manifests')
-end
