b7ab37e216e741ffd9077fda07713a2392382327
Jonatan Meneghetti <jonatan@unimestre.com>
2015-09-23 15:40:18 -0300

    Parâmetrizado os valores do comando moodle_auto

--- manifests/install/auto.pp
+++ manifests/install/auto.pp
@@ -1,22 +1,21 @@
-
 class moodle::install::auto () inherits moodle::params {
 
   exec{'moodle_auto':
     command => "/usr/bin/php admin/cli/install.php \
-      --dbtype=mysqli \
-      --dbname=moodledb \
-      --dbuser=fred \
-      --dbpass=fredblogs \
-      --adminpass=fredblogs \
+      --dbtype=$dbtype \
+      --dbname=$dbname \
+      --dbuser=$dbuser \
+      --dbpass=$dbpass \
+      --adminpass=$dbpass \
       --agree-license \
       --lang=en \
       --non-interactive \
-      --wwwroot=http://127.0.0.1:4568/moodle \
-      --dataroot=/opt/moodle \
+      --wwwroot=$wwwroot \
+      --dataroot=$dataroot \
       --fullname='test site' \
       --shortname=testsite",
-    creates => '/var/www/html/moodle/config.php',
-    cwd     => '/var/www/html/moodle',
+    creates => "/var/www/config.php",
+    cwd     => "/var/www/moodle",
     group   => 'apache',
   }
 }
